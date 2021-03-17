5dc532cdf58937024f9e3f210546a3d70dd85813
Steffan-debug <78175012+Steffan-debug@users.noreply.github.com>
2021-03-16 21:10:00 -0600

    Add files via upload

---
+++ 12/ex12.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+
+void InvertString (char(userInput[50])){
+	int i;
+	int largo = strlen(userInput)-1;
+	for(i=largo; i>=0; i--){
+		printf("%c %c", userInput[i],userInput[largo-i]);
+		if(userInput[i]==userInput[largo-i]){
+			printf("%i\n",1);
+		}else printf("%i\n",0);
+	}
+}
+	
+	int main(void) {
+		char userInput[70];
+		scanf("%s", userInput);
+		InvertString(userInput);
+		return 0;
+	}
