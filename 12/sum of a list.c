1b70241a6aab36a3f204fd4b5383508bcb8f8ad6
Steffan-debug <78175012+Steffan-debug@users.noreply.github.com>
2021-03-16 18:22:37 -0600

    Add files via upload

---
+++ 12/sum of a list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int sum(int arr[], int n)
+{
+    int sum = 0;
+ 
+    for (int i = 0; i < n; i++)
+    sum += arr[i];
+ 
+    return sum;
+}
+ 
+int main()
+{
+    int arr[] = {5, 7, 1, 180};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Sum of the list is %d", sum(arr, n));
+    return 0;
+}
